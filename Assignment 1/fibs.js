5f4eee437d384feaa766da87baffa15c8103b2b7
Azam <gulovazam@gmail.com>
2023-06-02 17:33:54 +0500

    Write pseudo code based on given instructions

---
+++ Assignment 1/fibs.js
@@ -0,0 +1,11 @@
+// Using iteration, write a function fibs which takes a number and
+// returns an array containing that many numbers from the fibonacci sequence.
+// Using an example input of 8, this function should return the array [0, 1, 1, 2, 3, 5, 8, 13].
+
+// pseudo-code:
+// The first two numbers in fibonacci is always 0 and 1
+// if the number is less than 0 throw error
+// if the number is less than 2 return it
+// each number is the sum of the two preceding ones
+// get the last two items from the array and add them
+// push result into the array
