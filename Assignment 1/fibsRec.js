923a2daf0b3dd103d2bc7036641f3e5657ef7f78
Azam <gulovazam@gmail.com>
2023-06-02 18:43:03 +0500

    Add recursive function

---
+++ Assignment 1/fibsRec.js
@@ -0,0 +1,21 @@
+// Instructions:
+// Now write another function fibsRec which solves the same problem recursively.
+// This can be done in just a couple of lines (or 1 if you’re crazy,
+// but don’t consider either of these lengths a requirement… just get it done).
+
+const fibsRec = (num, arr = [0, 1]) => {
+  if (num < 0) {
+    return "Please enter a whole number";
+  }
+
+  if (num < 2) {
+    return num;
+  }
+
+  const nextSeq = arr.at(-1) + arr.at(-2);
+  arr.push(nextSeq);
+  fibsRec(num - 1, arr);
+  return arr;
+};
+
+console.log(fibsRec(77));
