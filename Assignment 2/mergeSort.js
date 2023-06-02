d10ed548c405a3086d170d08da27dc2ab1952c45
Azam <gulovazam@gmail.com>
2023-06-02 19:08:21 +0500

    Write pseudo code for the helper function
    Declare merge helper function and initial variables
    Comment out testing output

--- Assignment 2/mergeSort.js
+++ Assignment 2/mergeSort.js
@@ -17,11 +17,28 @@ const mergeSort = (array) => {
   const rightHalf = array.slice(midPoint);
 
   // testing output
-  console.log(`Left half: ${leftHalf}`);
-  console.log(`Right half: ${rightHalf}`);
+  // console.log(`Left half: ${leftHalf}`);
+  // console.log(`Right half: ${rightHalf}`);
 
   mergeSort(leftHalf);
   mergeSort(rightHalf);
 };
 
 mergeSort([3, 5, 7, 15, 1, 4, 20]);
+
+// helper function
+
+// pseudo code:
+// take three arguments: left half, right half and the original array.
+// use three variables to keep track of left half, right half and the array.
+// keep loop going until numbers in either left half or right half end
+// if left half is less than right half, add it back to the original list
+// increment left half and the original array.
+// else increment right half and the original array.
+// check for remaining numbers in left half and right half.
+
+const merge = (leftHalf, rightHalf, array) => {
+  const i = 0;
+  const j = 0;
+  const k = 0;
+};
