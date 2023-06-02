55cae0faaddbb3a1c16e7a511d52857030c080a9
Azam <gulovazam@gmail.com>
2023-06-02 18:54:01 +0500

    Add instructions and pseudo code solution

---
+++ Assignment 2/mergeSort.js
@@ -0,0 +1,9 @@
+// Instructions
+// Build a function mergeSort that takes in an array and returns a sorted array, using a recursive merge sort methodology.
+
+// Pseudo code:
+// Use divide and conquer method
+// keep splitting array till its small enough to solve
+// sort the left half of the array
+// sort the right half of the array
+// merge them back into their original array
